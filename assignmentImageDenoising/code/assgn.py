448d7bcbd6def4d7237cc9036875b9f7cb59b88f
remag2069 <remag2069@gmail.com>
2022-02-04 00:17:56 +0530

    final

--- assignmentImageDenoising/code/assgn.py
+++ assignmentImageDenoising/code/assgn.py
@@ -3,7 +3,7 @@ import numpy as np
 import suppliment as s
 import cv2
 import matplotlib.pyplot as plt
-import scipy.io
+import scipy.io ## to read .mat file in python
 
 # quadratic- alpha=0.8
 # huber- alpha=0.8, gamma=0.1 0.33425
@@ -26,78 +26,87 @@ data = np.array(mat)
 cv2.imshow("Noisy",mat["imageNoisy"])
 cv2.imshow("Noiseless",mat["imageNoiseless"])
 
-def potential(image,mode="q",gamma=0.1):
+
+
+## defining the noise models and calling the prioirs to generate gradients
+def noise_model(image,mode="q",gamma=0.1):
     gradient=0
     loss=0
     
     if mode=="h":
+        ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in x
         for shift_index in [-1,1]:
-            # print(ALPHA)
             yi=np.roll(image,shift_index,1)
             new_grad,new_loss=s.huber(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses
-
+            loss+=new_loss*(1-ALPHA)     
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2) 
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     
+            
+            ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in y
             yi=np.roll(image,shift_index,0)
             new_grad,new_loss=s.huber(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses
+            loss+=new_loss*(1-ALPHA)     
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2) 
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     
 
     
     if mode=="r":
+        ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in x
         for shift_index in [-1,1]:
             yi=np.roll(image,shift_index,1)
             new_grad,new_loss=s.rice(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses
+            loss+=new_loss*(1-ALPHA)     
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2) 
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     
 
+            ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in y
             yi=np.roll(image,shift_index,0)
             new_grad,new_loss=s.rice(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses
+            loss+=new_loss*(1-ALPHA)     
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2) 
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     
 
     else:
+        ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in x
         for shift_index in [-1,1]:
             yi=np.roll(image,shift_index,1)
             new_grad,new_loss=s.quadratic(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses
-
+            loss+=new_loss*(1-ALPHA)     
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2) 
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     
+            ## using np.roll to emulate CIRCSHIFT, rolling on +-1 in y
             yi=np.roll(image,shift_index,0)
             new_grad,new_loss=s.quadratic(image-yi,gamma)
             gradient+=new_grad*(1-ALPHA)
-            loss+=new_loss*(1-ALPHA)                 ## 
-            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)  ## prior grads
-            loss+=ALPHA*(image-yi)**2/(SIGMA**2)     ## prior losses           
+            loss+=new_loss*(1-ALPHA)                  
+            gradient+=2*ALPHA*(image-yi)/(SIGMA**2)   
+            loss+=ALPHA*(image-yi)**2/(SIGMA**2)      
 
 
     return gradient,loss
 
 
 
-
+## dynamic step sizer, to change the step size through the epochs
 def dyn_step_sizer(STEP_SIZE,epoch):
     # print(STEP_SIZE)
     return STEP_SIZE-0.0495/2**epoch
 
 
-# for gamma in [x*0.4 for x in range(1,10)]:
+#######################CODE FOR FINDING OPTIMAL PARAM#######################
+
+# for gamma in [x*0.1 for x in range(1,10)]:
 #     for ALPHA in [x*0.1 for x in range(1,10)]:
 #         output=Y
 #         STEP_SIZE=0.1
 #         for epoch in range(100):
 #             STEP_SIZE=dyn_step_sizer(STEP_SIZE,epoch)
-#             gradient,loss=potential(output,"h",gamma)
+#             gradient,loss=noise_model(output,"h",gamma)
 #             # print(gradient)
 #             previous=output
 #             output=output-STEP_SIZE*gradient
@@ -113,70 +122,41 @@ def dyn_step_sizer(STEP_SIZE,epoch):
 
 #     break
 
-
-# for ALPHA in [0.5*0.8,0.5,0.5*1.2]:
-#     for gamma in [0.4*0.8,0.4,0.4*1.2]:
-        
-#         loss=inf
-
-#         output=Y
-#         prev_loss=0
-#         previous=0
-#         for epoch in range(100):
-#             prev_loss=loss
-#             gradient,loss=potential(output,"h",GAMMA)
-#             loss=np.sum(loss)
-#             LOSSES.append(loss)
-#             # if loss >= prev_loss:
-#                 # print(prev_loss)
-#             STEP_SIZE=dyn_step_sizer(STEP_SIZE,epoch)
-#             # else:
-#             #     STEP_SIZE*=1.1
-#             # print(STEP_SIZE)
-#             # print(gradient)
-#             previous=output
-#             output=output-STEP_SIZE*gradient
-            
-#             if s.rrmse(output,previous)<0.0001:
-#                 print("STOP EPOCH::::::",epoch)
-#                 break
-#             if epoch%10==0:
-#                 print("curr epoch=",epoch,"\nLoss=",loss)
-
-#         print(ALPHA,gamma,s.rrmse(output,X))
+#######################CODE FOR FINDING OPTIMAL PARAM#######################
 
 
-
-#         cv2.imshow("output_"+str(ALPHA)+"_"+str(gamma),output)
-#         # cv2.imwrite("output_1000epoch.jpg",output*255)
-
-# cv2.waitKey()
-
-
-# plt.plot(LOSSES)
-# plt.show()
-
-
-
-temp1=0.00001
-temp2=0.216
-
-for ALPHA in [temp1*0.8,temp1,temp1*1.2]:
-    for gamma in [temp2*0.8,temp2,temp2*1.2]:
-        output=Y
-        STEP_SIZE=0.1
-        for epoch in range(100):
-            STEP_SIZE=dyn_step_sizer(STEP_SIZE,epoch)
-            gradient,loss=potential(output,"r",gamma)
-            # print(gradient)
-            previous=output
-            output=output-STEP_SIZE*gradient
-            if s.rrmse(output,previous)<0.001:
-                print("EPOCH::::::",epoch)
-                break
+loss=inf
+output=Y
+prev_loss=0
+previous=0
+for epoch in range(100):
+    prev_loss=loss
+    gradient,loss=noise_model(output,"h",GAMMA)
+    loss=np.sum(loss)
+    LOSSES.append(loss)
+    # if loss >= prev_loss:
+        # print(prev_loss)
+    STEP_SIZE=dyn_step_sizer(STEP_SIZE,epoch)
+    # else:
+    #     STEP_SIZE*=1.1
+    # print(STEP_SIZE)
+    # print(gradient)
+    previous=output
+    output=output-STEP_SIZE*gradient
     
-        print(ALPHA,gamma,s.rrmse(output,X))
+    if s.rrmse(output,previous)<0.0001:
+        print("STOP EPOCH::::::",epoch)
+        break
+    if epoch%10==0:
+        print("curr epoch=",epoch,"\nLoss=",loss)
+print(ALPHA,GAMMA,s.rrmse(output,X))
+cv2.imshow("output_"+str(ALPHA)+"_"+str(GAMMA),output)
+cv2.imwrite("output.jpg",output*255)
 
-        # cv2.imshow("output",output)
-        cv2.imwrite("output_"+str(ALPHA*10)+str(gamma*10)+".jpg",output*255)
-        # cv2.waitKey()
+cv2.waitKey()
+
+## plotting the losses, through the epochs
+plt.plot(LOSSES)
+plt.show()
+
+
--- assignmentImageDenoising/code/suppliment.py
+++ assignmentImageDenoising/code/suppliment.py
@@ -1,31 +1,29 @@
 import numpy as np
 
+## quadratic prior model
 def quadratic(u,gamma=0):
     loss = np.sum(u**2)
-    # print(loss)
+
     gradient=2*abs(u)
-    # print(gradient)
     return gradient,loss
 
-
+## huber prior model
 def huber(u,gamma=0.1):
     loss = gamma*abs(u)-0.5*gamma**2
     loss[loss+0.5*gamma**2>gamma**2]=0.5*((loss[loss+0.5*gamma**2>gamma**2]+0.5*gamma**2)/gamma)**2
-    # print(loss)
+
     gradient=u
-    gradient[abs(gradient)<=gamma]=gamma*gradient[abs(gradient)<=gamma]/abs(gradient[abs(gradient)<=gamma])
-    
-    # print(gradient)
+    gradient[abs(gradient)<=gamma]=gamma*gradient[abs(gradient)<=gamma]/abs(gradient[abs(gradient)<=gamma])   
     return gradient,loss
 
-
+## discontinuity-adaptive functionprior model
 def rice(u,gamma=0.006):
     loss=gamma*abs(u)-(gamma**2)*np.log10(1+abs(u)/gamma)
     gradient=abs(u)/(1+abs(u)/gamma)*(u/abs(u))
     return gradient,loss
 
 
-
+## RRMSE loss
 def rrmse(a,b):
     error=np.sqrt(np.sum((a-b)**2)/np.sum(a**2))
     return error
