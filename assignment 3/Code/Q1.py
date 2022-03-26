d014c42fc781606d24fa79542ba457cb4e050559
remag2069 <remag2069@gmail.com>
2022-03-26 23:59:03 +0530

    n

--- assignment 3/Code/Q1.py
+++ assignment 3/Code/Q1.py
@@ -1,17 +1,13 @@
 import h5py
-# import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
 
-# with h5py.File('../data/hands2D (2).mat', 'r') as f:
-#     print(list(f.keys()))
-
 mat = h5py.File('../data/hands2D.mat')
 ITERS=5
 
-
+## start code 11 (assume standardized data, find rotation)
 def find_rotation(Z1,Z2):
     Z1=np.transpose(Z1)
     # print(Z1.shape,Z2.shape)
@@ -23,22 +19,20 @@ def find_rotation(Z1,Z2):
         temp=np.array([[1,0],[0,-1]])
         R=V*temp*np.transpose(U)
     
-    print("###############################",R)
     return R
+## end code 1 (assume standardized data, find rotation)
 
 shapes=mat["shapes"]
 
-# for i in range(40):
-#     plt.scatter(shapes[i,:,0], shapes[i,:,1],s=5)
+for i in range(40):
+    plt.scatter(shapes[i,:,0], shapes[i,:,1],s=5)
 
-# for i in range(ITERS):
-#     for j in range()
+plt.title("Initial pointsets")
+plt.show()
+
+
 
 centroid=np.mean(shapes,axis=1).reshape(-1,1,2)
-# temp=np.tile(centroid,[1,56,1])
-
-# print(centroid.reshape(2,-1))
-# print(centroid)
 preshapePointSets=shapes-centroid
 norm=np.sqrt(np.sum(np.sum(preshapePointSets**2,axis=1),axis=1)).reshape(-1,1,1)
 preshapePointSets=preshapePointSets/norm
@@ -50,34 +44,52 @@ mean_shape=preshapePointSets[0,:,:]
 Errors=[]
 
 for iter in range(ITERS):
+    ## start code 11
     for i in range(40):
         R=find_rotation(preshapePointSets[i,:,:],mean_shape)
 
         preshapePointSets[i,:,:]=np.transpose(np.matmul(R,np.transpose(preshapePointSets[i,:,:])))
 
-    # print(preshapePointSets[23,34,1])
+    ## end code 11
 
+    
+
+    ## start code 22 
     new_mean_shape = np.mean(preshapePointSets,axis=0)
     norm=np.sqrt(np.sum(new_mean_shape**2))
-    print("noooorm",new_mean_shape)
     new_mean_shape = new_mean_shape/norm
 
+    ## end code 22 
+
     error = np.sqrt(np.sum((new_mean_shape - mean_shape)**2))
-    # print("###########",iter,norm,error)
     Errors.append(error)
 
     mean_shape=new_mean_shape
 
+
+
+
+
+## plot standardized data with mean
+plt.plot(mean_shape[:,0],mean_shape[:,1])
+for i in range(40):
+    plt.scatter(preshapePointSets[i,:,0], preshapePointSets[i,:,1],s=5)
+plt.title("sample mean with aligned pointset")
 plt.show()
 
-print("~~~~~~~~~~~~~~~~~",preshapePointSets[0,:,:])
-print(Errors)
+new_shapes=(preshapePointSets-mean_shape).reshape(2*56,40)
+covariance = np.matmul(new_shapes,np.transpose(new_shapes))/40
 
+D,V=np.linalg.eig(covariance)
 
-# plt.plot(mean_shape[:,0],mean_shape[:,1])
-# for i in range(40):
-#     plt.scatter(preshapePointSets[i,:,0], preshapePointSets[i,:,1],s=5)
+idx = D.argsort()[::-1]   
+eigenValues = np.real(D[idx])
+eigenVectors = np.real(V[:,idx])
 
+pm31 = mean_shape + 2 * (np.sqrt(eigenValues[2])*eigenVectors[:,2]).reshape(-1,2)
+pm31 = mean_shape + 2 * (np.sqrt(eigenValues[2])*eigenVectors[:,2]).reshape(-1,2)
 
-
+plt.plot(pm31[:,0], pm31[:,1])
+plt.legend("")
+plt.title("")
 plt.show()
